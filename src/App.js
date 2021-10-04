f7af51255b2c3ac8270b9ca10dacd14c8d5f82da
Betsy Vidal <ing.betsyvies@gmail.com>
2021-10-03 21:46:49 -0500

    feat(components): Add BlogPost Component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import BlogPost from './BlogPost';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BlogPost />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/BlogPost.js
@@ -0,0 +1,12 @@
+const BlogPost = () => {
+  return (
+    <div>
+      <h1>Blog</h1>
+      <p>Explore the unknown. Uncover what matters. Prototype, test, repeat. Combine intuition with evidence. Design with intent and build it right.</p>
+      <input></input>
+      <button>Search</button>
+    </div>
+  );
+}
+
+export default BlogPost;
