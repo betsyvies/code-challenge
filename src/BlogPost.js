fa16a12d98880b2fb65a4bea8d29a1f53729234f
Betsy Vidal <ing.betsyvies@gmail.com>
2021-10-03 22:00:30 -0500

    feat(components): Add handleChange and handleSearch functions in BlogPost Component

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,10 +1,30 @@
+import { useState } from 'react';
+
 const BlogPost = () => {
+  const [userName, setUserName] = useState('');
+
+  const handleChange = (e) => {
+    const name = e.target.value;
+    setUserName(name);
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    console.log(userName);
+  };
+
   return (
     <div>
       <h1>Blog</h1>
       <p>Explore the unknown. Uncover what matters. Prototype, test, repeat. Combine intuition with evidence. Design with intent and build it right.</p>
-      <input></input>
-      <button>Search</button>
+      <form onSubmit={handleSearch}>
+        <label htmlFor="userName">
+          <input type="text" name="userName" onChange={handleChange} />
+        </label>
+        <div>
+          <button type="submit">Search</button>
+        </div>
+      </form>
     </div>
   );
 }
